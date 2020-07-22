5549eec008b381c9b62ce53e7c8ca51db3f5e473
mohit2308jain <mohitjain5000@gmail.com>
2020-07-22 18:36:00 +0530

    API

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 //See Text file
-const KEY = 'AIzaSyDp8N8eQ7KE_8h3Pn8QvzWMh3dbtVkCQc8';
+const KEY = process.env.API_KEY;
 
 export const baseParams = {
     part: "snippet",
